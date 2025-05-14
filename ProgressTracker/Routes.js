5d6e5f07a1eaf3520fe725eb0c0ed1c7f52bc25a
Iqbal <108946263+Iqbalenter@users.noreply.github.com>
2025-05-15 01:06:26 +0700

    Update Routes.js

--- ProgressTracker/Routes.js
+++ ProgressTracker/Routes.js
@@ -1,9 +1,10 @@
-const { addProgress, updateProgressTitle } = require('./HandlerProgress');
+const { addProgress, updateProgressTitle, getProgress } = require('./HandlerProgress');
 const express = require('express');
 const router = express.Router();
 const validateToken = require('../utils/validateToken');
 
 router.post('/progress', validateToken, addProgress);
 router.put('/progress/update-title', validateToken, updateProgressTitle);
+router.get('/progress/get', validateToken, getProgress);
 
 module.exports = router;
